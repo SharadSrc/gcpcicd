d9257cb327b5d8b2f05d81dc20922fa1fc722133
SharadSrc <sd@gmail.com>
2023-04-03 01:11:57 +0530

    commit

---
+++ src/main/java/com/sd/cicdgoogle/GCPCICD/GcpcicdApplication.java
@@ -0,0 +1,13 @@
+package com.sd.cicdgoogle.GCPCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpcicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpcicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sd/cicdgoogle/GCPCICD/GcpcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd.cicdgoogle.GCPCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpcicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
